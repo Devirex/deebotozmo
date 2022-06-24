a14bb50f9866b52565afbf968f28ead3bf603ac0
Devirex <naibaf_11@yahoo.de>
2022-06-24 09:01:19 +0200

    add lib to manifest

--- deebotozmo-fhem.py
+++ deebotozmo-fhem.py
@@ -4,15 +4,13 @@ import uuid, base64
 from cryptography.fernet import Fernet
 import cryptography
 import debugpy
-import sys
-sys.path.insert(0, "../deebotozmo")
-from deebotozmo.ecovacs_api import EcovacsAPI
-from deebotozmo.ecovacs_mqtt import EcovacsMqtt
-from deebotozmo.events import BatteryEvent
+from ..deebotozmo.ecovacs_api import EcovacsAPI
+from ..deebotozmo.ecovacs_mqtt import EcovacsMqtt
+from ..deebotozmo.events import BatteryEvent
 import aiohttp
 from aiohttp import ClientError
-from fhempy.lib.deebotozmo.util import md5
-from fhempy.lib.deebotozmo.vacuum_bot import VacuumBot
+from ..deebotozmo.util import md5
+from ..deebotozmo.vacuum_bot import VacuumBot
 debugpy.listen(("192.168.1.50",5678))
 
 

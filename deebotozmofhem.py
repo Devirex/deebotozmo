f2627ec5892b1eb624e147aec566326dd1477ebf
Devirex <naibaf_11@yahoo.de>
2022-07-01 16:53:06 +0200

    add lib to manifest

--- deebotozmofhem.py
+++ deebotozmofhem.py
@@ -6,11 +6,11 @@ import cryptography
 import debugpy
 import aiohttp
 from aiohttp import ClientError
-from ...Deebotozmo.deebotozmo.ecovacs_api import EcovacsAPI
-from ...Deebotozmo.deebotozmo.ecovacs_mqtt import EcovacsMqtt
-from ...Deebotozmo.deebotozmo.events import BatteryEvent
-from ...Deebotozmo.deebotozmo.vacuum_bot import VacuumBot
-from ...Deebotozmo.deebotozmo.util import md5
+from .Deebotozmo.deebotozmo.ecovacs_api import EcovacsAPI
+from .Deebotozmo.deebotozmo.ecovacs_mqtt import EcovacsMqtt
+from .Deebotozmo.deebotozmo.events import BatteryEvent
+from .Deebotozmo.deebotozmo.vacuum_bot import VacuumBot
+from .Deebotozmo.deebotozmo.util import md5
 debugpy.listen(("192.168.1.50",5678))
 
 

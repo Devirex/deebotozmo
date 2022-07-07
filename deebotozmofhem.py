4c24ac27dab8d3031be3bb7799795ac4d81484aa
Devirex <naibaf_11@yahoo.de>
2022-07-07 16:56:50 +0200

    typo

--- deebotozmofhem.py
+++ deebotozmofhem.py
@@ -147,7 +147,7 @@ class deebotozmofhem(generic.FhemModule):
         async def on_map(event: MapEvent):
             # Do stuff on battery event
             # Battery full
-            await fhem.readingsSingleUpdate(self.hash, "Map" , '<img src="data:image/png;base64;' + base64.decodebytes(bot.map.get_base64_map(400)) + '"/>', 1)
+            await fhem.readingsSingleUpdate(self.hash, "Map" , '<img src="data:image/png;base64;' + base64.decodebytes(bot.map.get_base64_map(400)).decode('utf-8') + '"/>', 1)
             pass
         
         bot.events.map.subscribe(on_map)
